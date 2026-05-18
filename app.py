f65b16d451c69c5c82ef12f12efd5718246bf72e
Youssef Ali <154611350+YoussefKhalidAli@users.noreply.github.com>
2026-05-18 18:28:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import threading
+import time
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -7,7 +9,18 @@ class Handler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"Hello from GitHub! Attempt1")
 
+
+def log_message():
+    while True:
+        print("Server is still running...")
+        time.sleep(10)
+
+
 if __name__ == "__main__":
+    # Start logger thread
+    threading.Thread(target=log_message, daemon=True).start()
+
     server = HTTPServer(("0.0.0.0", 8000), Handler)
     print("Server running on port 8000")
+
     server.serve_forever()
