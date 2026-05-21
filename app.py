f6b2bbf466f9db7cb3b1b723a86a554ef8c77337
Youssef Ali <154611350+YoussefKhalidAli@users.noreply.github.com>
2026-05-21 16:29:42 +0300

    15

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ class Handler(BaseHTTPRequestHandler):
 
 def log_message():
     while True:
-        print("images are defo (fr*14) not in zot.registry.svc.cluster.local", flush=True)
+        print("images are defo (fr*15) not in zot.registry.svc.cluster.local", flush=True)
         time.sleep(10)
 
 
